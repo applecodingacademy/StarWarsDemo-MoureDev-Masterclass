c4cf8db224aef8aca0556f74e1df626162707672
Julio César Fernández <juliocesar.fernandez@gabhel.com>
2024-09-03 19:49:11 +0200

    Initial Commit

---
+++ StarWarsDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StarWarsDemo
+//
+//  Created by Julio César Fernández Muñoz on 3/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StarWarsDemo/StarWarsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsDemoApp.swift
+//  StarWarsDemo
+//
+//  Created by Julio César Fernández Muñoz on 3/9/24.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
